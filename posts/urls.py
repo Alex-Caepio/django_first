e2b9a99c926fb963a0df4f42f41fd2da89ff03d3
Aleksei Brutov <mjbcaepio@gmail.com>
2023-09-29 18:59:50 +0300

    Сразу две домашки в одном коммите. Особое внимание уделено работе с методом filter().

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,13 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.my_posts, name='posts')
+    path('all/<int:post_id>', views.my_posts, name='posts'),
+    path('new/', views.create_post, name='create_post'),
+    path('<int:post_id>/', views.get_one_post, name='get_one_post'),
+    path('<int:post_id>/exists/', views.check_if_exists, name='check_if_exists'),
+    path('<int:post_id>/update/', views.update_post, name='update_post'),
+    path('<int:post_id>/delete/', views.delete_post, name='delete_post'),
+    path('<int:post_id>/get_or_create/', views.get_or_create, name='get_or_create'),
+    path('<int:post_id>/update_or_create/', views.update_or_create, name='update_or_create'),
+    path('filter_and_update/', views.filter_and_update, name='filter_and_update'),
 ]
