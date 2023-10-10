46a22872901a847594cfc76d55282ce2de7b361b
Aleksei Brutov <mjbcaepio@gmail.com>
2023-10-10 21:37:28 +0300

    Сделал сигнал и обработчик, представляющий собой функцию, отслеживающую количество записей в бд с одинаковым значенией в поле "категория".

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,40 @@
+from datetime import datetime
 from django.db import models
+from django.db.models import signals
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
+    category = models.CharField(max_length=100, default="")
+    created_at = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
         return self.title
+
+
+# @receiver(signals.pre_save, sender=Post)
+# def post_save_handler(sender, instance, **kwargs):
+#     raise Exception('Error')
+
+
+# pre_save.connect(post_save_handler, sender=Post)
+
+@receiver(signals.post_save, sender=Post)
+def post_save_handler(sender, instance, **kwargs):
+    if Post.objects.filter(category=instance.category).count() >= MAX_COUNT_PER_CATEGORY.get(instance.category,
+                                                                                             DEFAULT_MAX_COUNT):
+        Post.objects.filter(category=instance.category).order_by('created_at').first().delete()
+        print("Deleted!")
+
+
+MAX_COUNT_PER_CATEGORY = {
+    'science': 2,
+    'math': 3,
+    'history': 4,
+}
+
+DEFAULT_MAX_COUNT = 5
